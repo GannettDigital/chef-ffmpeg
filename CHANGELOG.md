773d6399cdef3ba3358231bccd6785a882bbb65c
Jason Neves <jneves@gannett.com>
2015-09-08 18:25:09 -0400

    GDAES-1917 - missing email in metadata

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## v1.0.1:
+
+-- Jason Neves - Fix missing email in metadata
+
 ## v1.0.0:
 
 -- Jason Neves - Rebuild of cookbook to add package install method and testing
--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jneves@gannett.com'
 license	         'All rights reserved'
 description      'Installs ffmpeg'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version	         '1.0.0'
+version	         '1.0.1'
 
 depends 'yum-gd'
 depends 'build-essential'
